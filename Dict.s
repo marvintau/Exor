2c9cbd0c09858bd569fb445a80bf990004575a3c
marvinax <nightvista@gmail.com>
2016-08-27 18:38:45 +0800

    documented registers

--- Dict.s
+++ Dict.s
@@ -18,11 +18,27 @@ DictEnd:
 # that create a REPL environment to define and run
 # words.
 
+# The words in the three modules are highly coupled,
+# Thus the registers are not passed through stack,
+# described as below:
+
+# r8 & r9: Word starting address in input buffer,
+#          and word length
+# r10:     Return stack pointer, initialized and
+#          operated by macros defined in Stack.s
+# r11:     Entry pointer, operated by traversing
+#          routines defined in Find.s
+# r12:     Entry pointer for indirect addressing,
+#          always points to an address that holds
+#          an address that locates executable code.
+# r13:     Pointer to the address that holds the
+#          NEXT address that to be copied to r12
+
     .include "Lexer.s"
     .include "Find.s"
     .include "Execute.s"
+    .include "Define.s"
 
     .include "StringDisplay.s"
     .include "Parse.s"
-    .include "Define.s"
 DictStart:
