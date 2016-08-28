592495a3cc6a48e00a608f6b5c30738e295b4bcc
marvinax <nightvista@gmail.com>
2016-08-28 23:17:46 +0800

    changed documentation

--- Dict.s
+++ Dict.s
@@ -1,39 +1,34 @@
+# ========================================================================
 # Dictionary.s
-# ================================================
-# Dictionary holds all entries. The Macros defined
-# in EntryStructure.s enables creating entries and
-# forms a linked table, which holds code entries,
-# and word entries consisting of other entries.
+# ========================================================================
+# Dictionary holds all entries. The Macros defined in EntryStructure.s
+# enables creating entries and forms a linked table, which holds different
+# types of entries.
 
 .include "EntryStructure.s"
 
 DictEnd:
     .quad 0x000000000000
 
-# ================================================
-# LEXER, FIND & EXECUTE
-# ================================================
-# The foundation of Exor, the basic code and words
-# that create a REPL environment to define and run
-# words.
+# ========================================================================
+# LEXER, FIND, EXECUTE & DEFINE
+# ========================================================================
+# The foundation of Exor, consisting of basic REPL environment and tools
+# for defining and executing words.
 
-# The words in the three modules are highly coupled,
-# Thus the registers are not passed through stack,
-# described as below:
+# The words in the three modules are highly coupled, thus the registers
+# are not passed through stack, described as below:
 
-# r8 & r9: Word starting address in input buffer,
-#          and word length
-# r10:     Return stack pointer, initialized and
-#          operated by macros defined in Stack.s
-# r11:     Entry pointer, operated by traversing
-#          routines defined in Find.s
-# r12:     Entry pointer for indirect addressing,
-#          always points to an address that holds
-#          an address that locates executable code.
-# r13:     Pointer to the address that holds the
-#          NEXT address that to be copied to r12
-# r14:     The counter that used for recording 
-#          how many words have been compiled.
+# r8 & r9: Word starting address in input buffer, and word length
+# r10:     Return stack pointer, initialized and operated by macros that
+#          defined in Stack.s
+# r11:     Entry pointer, operated by traversing routines defined in Find.s
+# r12:     Entry pointer for indirect addressing, always pointing to an
+#          address that holds an address that locates executable code.
+# r13:     Pointer to the address that holds the NEXT address that to be
+#          copied to r12
+# r14:     The counter that used for recording how many words have been
+#          compiled.
 
     .include "Execute.s"
     .include "BuiltinEntries.s"
